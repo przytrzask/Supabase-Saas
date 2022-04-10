b55eb67f5f77aa3d85c237db762698b5b36ab168
Tomasz Trzaskoma <tomasz.trzaskoma@brainly.com>
2022-04-10 18:42:12 +0200

    types

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,31 @@
-import Head from "next/head";
+import { PostgrestResponse } from "@supabase/supabase-js";
+
 import { supabase } from "../utils/supabase";
 
+import { definitions } from "../types/supabase";
+
 type Props = {
-  lessons: any;
+  lessons: PostgrestResponse<definitions["lessons"]>;
 };
 
 export default function Home({ lessons }: Props) {
   console.log({ lessons });
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      working
+      {lessons.body.map((lesson) => (
+        <p>
+          {lesson.title}
+          {lesson.description}
+        </p>
+      ))}
     </div>
   );
 }
 
 export async function getStaticProps() {
-  const lessons = await supabase.from("lessons").select("*");
+  const lessons = await supabase
+    .from<definitions["lessons"]>("lessons")
+    .select("*");
 
   return {
     props: {
